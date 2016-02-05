93a08871b08d1901bd276f4c6cfc667cb2b2de22
mabdh <a_bduh@yahoo.com>
2016-02-05 22:26:26 +0100

    add sample mid

--- WAVTOMIDI_fft.py
+++ WAVTOMIDI_fft.py
@@ -107,12 +107,12 @@ def fundamental_freq_fft(signal,sampleRate):
     print(n)
 
     # To quantify energy
-    frame_ms = 2
+    frame_ms = 1
     frame_s = frame_ms / 1000.0
     frame_samples = int(frame_s * sampleRate)
     # frame_samples = 1024
     # percent %
-    overlap = 0.5
+    overlap = 0
     overlap_samples = int(overlap * frame_samples)
     downsampleorder = 100
     i = 0
@@ -207,6 +207,17 @@ def fundamental_freq_fft(signal,sampleRate):
         diff = len(freq) - len(freq_d3)
         freq_d3 = np.lib.pad(freq_d3, (0,diff),'constant', constant_values=(0,0))
 
+
+        freq[:20] = 0
+        freq_d2[:20] = 0
+        freq_d3[:20] = 0
+
+        f, (ax1, ax2, ax3) = plt.subplots(3, sharex=True, sharey=True)
+        ax1.plot(freq)
+        ax1.set_title('Sharing both axes')
+        ax2.plot(freq_d2)
+        ax3.plot(freq_d3)
+
         mul1 = np.multiply(freq,freq_d2)
         mul2 = np.multiply(mul1,freq_d3)
 
@@ -234,127 +245,6 @@ def fundamental_freq_fft(signal,sampleRate):
     print(freqArray)
     return freqArray, tick_indices
 
-def fundamental_freq_fft_windowed(signal,sampleRate):
-    n = len(signal)
-    print(n)
-
-    # To quantify energy
-    frame_ms = 2
-    frame_s = frame_ms / 1000.0
-    frame_samples = int(frame_s * sampleRate)
-    # frame_samples = 1024
-    # percent %
-    overlap = 0
-    overlap_samples = int(overlap * frame_samples)
-    downsampleorder = 100
-    i = 0
-    freqArray = []
-
-    #calculate energy
-    energyStream = []
-    while(i < n):
-    	signal_sample = signal[i:i+frame_samples]
-    	n_sig_sample = len(signal_sample)
-    	Nfft = n_sig_sample
-    	windowed = signal_sample * hann(n_sig_sample, Nfft)
-    	en = getEnergy(windowed)
-    	energyStream.append(en)
-    	i = i + frame_samples - overlap_samples
-
-    #threshold
-    meanEnergy = np.sum(energyStream)/len(energyStream)
-    energyTh = meanEnergy/4
-
-    for i in range(len(energyStream)):
-    	if energyStream[i] < energyTh:
-    		energyStream[i] = 0.0
-    # print(energyTh)
-
-    #segmenting
-    energyStream = np.cumsum(energyStream)
-    energyStream = energyStream[0::downsampleorder]
-    energyStream = np.gradient(np.gradient(energyStream))
-
-    #get indices with zero crossing
-    indices = []
-    indices = np.where(((energyStream[1:] >= 0) & (energyStream[:-1] < 0)) | ((energyStream[1:] < 0) & (energyStream[:-1] >= 0)))[0]
-    if(len(indices)%2!=0):
-    	indices = indices[0:len(indices)-1]
-    
-    indices = np.split(indices,int(len(indices)/2))
-    print(n/frame_samples)
-    print(len(energyStream))
-    print(indices)
-    # for i in range(1,len(energyStream)-1):
-    	# if ((energyStream[i-1] < 0) and (energyStream[i+1] > 0)) or ((energyStream[i-1] > 0) and (energyStream[i+1] < 0)):
-    	# 	indices.append(i)
-
-    # plt.figure(1)
-    # plt.plot(energyStream)
-    # plt.figure(2)
-    # plt.plot(signal)
-    # plt.figure(3)
-    # plt.step(np.arange(len(indices)),indices)
-    # plt.show()
-    # print(energyStream)
-    
-    multiplier = downsampleorder * frame_samples
-
-    for idx in indices:
-    	idx = idx * multiplier
-    	istart = idx[0]
-    	frame_samples = idx[1] - idx[0] 
-
-    	if istart+frame_samples >= n:
-    		frame_samples = n - istart
-
-    	signal_sample = signal[istart:istart+frame_samples]
-    	n_sig_sample = len(signal_sample)
-
-    	# plt.figure(5)
-    	# plt.plot(signal_sample)
-    	# plt.show()
-
-    	Nfft = n_sig_sample
-    	windowed = signal_sample * hann(n_sig_sample, Nfft)
-    	freq = rfft(windowed, Nfft)    
-    	freq_d2 = rfft(windowed, Nfft)
-    	freq_d3 = rfft(windowed, Nfft)
-
-    	freq_d2 =  freq_d2[0::2]
-    	diff = len(freq) - len(freq_d2)
-    	freq_d2 = np.lib.pad(freq_d2, (0,diff),'constant', constant_values=(0,0))
-    	freq_d3 =  freq_d3[0::3]
-    	diff = len(freq) - len(freq_d3)
-    	freq_d3 = np.lib.pad(freq_d3, (0,diff),'constant', constant_values=(0,0))
-
-    	mul1 = np.multiply(freq,freq_d2)
-    	mul2 = np.multiply(mul1,freq_d3)
-
-    	freqF0 = argmax(mul2) * (sampleRate / n_sig_sample)
-    	
-    	if(freqF0 > 0.0):
-    		freqF0 = int(round(69 + 12 * math.log(freqF0 / 440.0, 2)))
-    	else:
-    		freqF0 = 0
-    	print(freqF0)
-    	freqArray.append(freqF0)
-    	# i = i + frame_samples - overlap_samples
-    plt.figure(1)
-
-    # freqAxis = arange(0, len(freq), 1.0) * (sampleRate / len(freq))
-    # plt.plot(freqAxis/1000, 10*log10(freq))
-    # plt.plot(freqArray)
-    # plt.xlabel('k-sample')
-    # plt.ylabel('Frequency (Hz)')
-    
-    # plt.figure(2)
-    # plt.plot(signal)
-    # plt.show()
-    print(freqArray)
-    return freqArray
-
-
 def show_spectrogram(x, fs):
 	plt.figure(12)
 	Pxx, freqs, bins, im = plt.specgram(x, NFFT=1024, Fs=fs, noverlap=900,
@@ -364,13 +254,15 @@ def show_spectrogram(x, fs):
 
 # stream = open("wavFile/Berklee/piano_C2.wav")
 # stream = open("wavFile/gnote.mp3")
-stream = open("wavFile/C4humm.wav")
+stream = open("wavFile/")
+stream = open("wavFile/gundulpacul.wav")
 
 # partStream = stream[0:(int(len(stream)*0.3))]
 # plt.plot(stream)
 # plt.show()
 # plot_fft(stream,44100)
 sampleRate = 44100
+# sampleRate = 8000
 # show_spectrogram(stream,sampleRate)
 
 freqArray, tick_ms = fundamental_freq_fft(stream,sampleRate)
