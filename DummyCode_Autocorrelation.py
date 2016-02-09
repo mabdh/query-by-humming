57cf9056ed9fd772ebd3562c4e0cd25ca8e78850
Maureen Tanuadji <maureen.tanuadji@dfki.de>
2016-02-09 01:15:10 +0100

    Modified version

--- DummyCode_Autocorrelation.py
+++ DummyCode_Autocorrelation.py
@@ -9,15 +9,12 @@ import matplotlib.pyplot as plt
 from scipy import signal
 
 
-# In[71]:
+# In[137]:
 
-def autocorr(x, t=1):
-    x = np.asarray(x)
-    return np.corrcoef([x[0:len(x)-t], x[t:len(x)]])
 def EstimatePeriod( x, n, minP, maxP, q ):
-    nac = np.empty(maxP+1)
+    nac = np.empty(maxP+2)
     x = np.asarray(x)
-    for p in range(minP-1,maxP+1):
+    for p in range(minP-1,maxP+2):
         ac = 0.0
         sumSqBeg = 0.0
         sumSqEnd = 0.0
@@ -27,20 +24,40 @@ def EstimatePeriod( x, n, minP, maxP, q ):
             sumSqBeg += x[i]*x[i]
             sumSqEnd += x[i+p]*x[i+p]
         nac[p] = ac / np.sqrt( sumSqBeg * sumSqEnd )
-    print(len(nac))
+    
     bestP = np.amax([minP,np.argmax(nac)])
+    
     if(nac[bestP]<nac[bestP-1] and nac[bestP]<nac[bestP+1]):
         return (0.0, 0)
+    
     q = nac[bestP]
+
     mid = nac[bestP]
     left = nac[bestP-1]
     right = nac[bestP+1]
     shift = 0.5*(right-left) / ( 2*mid - left - right )
     pEst = bestP + shift
-    return (pEst,bestP)
+
+    k_subMulThreshold = 0.90
+    maxMul = int(bestP / minP)
+
+    found = False
+    mul = maxMul
+    while((not found) and mul>=1):
+        subsAllStrong = True
+        for k in range(1,mul):
+            subMulP = int(k*pEst/mul+0.5)
+            if ( nac[subMulP] < k_subMulThreshold * nac[bestP] ):
+                subsAllStrong = False
+        if(subsAllStrong):
+            found = True
+            pEst = pEst / mul
+        mul -=1
+    
+    return pEst, q
 
 
-# In[72]:
+# In[139]:
 
 eps = 1e-50
 sr = 44100
@@ -50,8 +67,10 @@ minP = int(sr/maxF-1)
 maxP = int(sr/minF+1)
 
 A440 = 440.0
-f = A440 * np.power(2.0,-9.0/12.0)
+#f = A440 * np.power(2.0,-9.0/12.0)
+f = A440
 p = sr/f
+print(p)
 n = 2*maxP
 q = 1
 x = np.empty(n)
@@ -60,23 +79,25 @@ for k in range(0,n):
     x[k] += 1.0*np.sin(2*np.pi*1*k/p)
     x[k] += 0.6*np.sin(2*np.pi*2*k/p)
     x[k] += 0.3*np.sin(2*np.pi*3*k/p)
-pEst = np.max(EstimatePeriod( x, n, minP, maxP, q ),eps)
+
+(pEst,q) = EstimatePeriod( x, n, minP, maxP, q )
+pEst = np.max([pEst,eps])
 fEst = eps
 if ( pEst > 0 ):
     fEst = sr/pEst
     
 print( "Actual freq:         %8.3lf\n"% f );
-print( "Estimated freq:      %8.3lf\n"% float(sr/pEst) );
+print( "Estimated freq:      %8.3lf\n"% float(fEst) );
 print( "Error (cents):       %8.3lf\n"% float(100*12*np.log(fEst/f)/np.log(2)) );
 print( "Periodicity quality: %8.3lf\n"% q );
 
 
-# In[74]:
+# In[ ]:
 
 
 
 
-# In[75]:
+# In[ ]:
 
 
 
